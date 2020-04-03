3b4606edfb9499527c497d3f38b3e2ee16b53b40
Artur Polo Norte <arturcastropolonorte@gmail.com>
2020-04-03 08:04:29 -0300

    importando e editando
    provavelmente alguns arquivos importadoss estão iguais re-avaliar

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,22 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
@@ -143,7 +160,6 @@ public class Controller implements Initializable {
         return motocicletaList;
     }
 
-    // I had to change ArrayList to ObservableList I didn't find another option to do this but this works :)
     public void showMotocicletas() {
         ObservableList<Motocicleta> list = getMotocicletaList();
 
